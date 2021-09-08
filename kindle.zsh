d0558d59b784c93abf769ff94d61c0e41590dbca
Gabriel Salas <ggsalas@gmail.com>
2021-09-08 20:22:41 -0300

    add blogs receipe

--- kindle.zsh
+++ kindle.zsh
@@ -5,4 +5,4 @@ source $KINDLE_HOME_DIR/src/app.sh
 alias kindle='getRecipe'
 alias kindle-tr='testRecipe'
 alias kindle-te='testEmail'
-alias kindle-weekly='getRecipe perfil lacapital clarin js politica world'
+alias kindle-weekly='getRecipe perfil lacapital clarin js blogs'
