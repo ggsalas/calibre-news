8b33bc525bf73067927c664215f6c55dc0b0613b
Gabriel Salas <ggsalas@gmail.com>
2021-09-24 21:25:53 -0300

    Add blogs recipe

--- getDailyPublications.sh
+++ getDailyPublications.sh
@@ -2,11 +2,11 @@
 
 if [ $(date +%u) -eq 6 ]; then
   echo ">>>>>>>>>>>>>>>>>>>>> Get Saturday ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
-  sh $KINDLE_HOME_DIR/kindle.sh lacapital js politica world
+  sh $KINDLE_HOME_DIR/kindle.sh lacapital perfil blogs
 
 elif [ $(date +%u) -eq 7 ]; then
   echo ">>>>>>>>>>>>>>>>>>>>> Get Sunday ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
-  sh $KINDLE_HOME_DIR/kindle.sh perfil lacapital clarin js
+  sh $KINDLE_HOME_DIR/kindle.sh perfil clarin 
 
 else
   echo ">>>>>>>>>>>>>>>>>>>>> No News for today ($(date +%u)) <<<<<<<<<<<<<<<<<<<<<"
