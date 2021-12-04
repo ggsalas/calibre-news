b439a654753b58139a708b6caace6e238d158b58
Gabriel Salas <ggsalas@gmail.com>
2021-12-03 22:23:47 -0300

    Replace perfil by lanacion

--- getDailyPublications.sh
+++ getDailyPublications.sh
@@ -2,11 +2,11 @@
 
 if [ $(date +%u) -eq 6 ]; then
   echo ">>>>>>>>>>>>>>>>>>>>> Get Saturday ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
-  sh $KINDLE_HOME_DIR/kindle.sh lacapital perfil blogs
+  sh $KINDLE_HOME_DIR/kindle.sh lacapital lanacion blogs
 
 elif [ $(date +%u) -eq 7 ]; then
   echo ">>>>>>>>>>>>>>>>>>>>> Get Sunday ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
-  sh $KINDLE_HOME_DIR/kindle.sh perfil clarin 
+  sh $KINDLE_HOME_DIR/kindle.sh lanacion clarin 
 
 else
   echo ">>>>>>>>>>>>>>>>>>>>> No News for today ($(date +%u)) <<<<<<<<<<<<<<<<<<<<<"
