3403b932452214343a3155feacc8eb299f1e7aa0
Gabriel Salas <ggsalas@gmail.com>
2022-10-02 11:29:41 +0200

    feat: Update perfil recepie & move to epub format

--- getDailyPublications.sh
+++ getDailyPublications.sh
@@ -2,11 +2,11 @@
 
 if [ $(date +%u) -eq 6 ]; then
   echo ">>>>>>>>>>>>>>>>>>>>> Get Saturday ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
-  sh $KINDLE_HOME_DIR/kindle.sh lacapital lanacion blogs
+  sh $KINDLE_HOME_DIR/kindle.sh lacapital lanacion perfil blogs
 
 elif [ $(date +%u) -eq 7 ]; then
   echo ">>>>>>>>>>>>>>>>>>>>> Get Sunday ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
-  sh $KINDLE_HOME_DIR/kindle.sh lanacion clarin 
+  sh $KINDLE_HOME_DIR/kindle.sh lanacion clarin perfil
 
 else
   echo ">>>>>>>>>>>>>>>>>>>>> No News for today ($(date +%u)) <<<<<<<<<<<<<<<<<<<<<"
--- src/app.sh
+++ src/app.sh
@@ -12,19 +12,19 @@ getRecipe() {
 
   for var in "$@"
   do
-    ebook-convert $KINDLE_HOME_DIR/src/recipes/$var.recipe $KINDLE_HOME_DIR/books/$var.mobi --output-profile kindle
-    sendEmail $KINDLE_HOME_DIR/books/$var.mobi $var
+    ebook-convert $KINDLE_HOME_DIR/src/recipes/$var.recipe $KINDLE_HOME_DIR/books/$var.epub --output-profile kindle
+    sendEmail $KINDLE_HOME_DIR/books/$var.epub $var
   done
 }
 
 testRecipe() {
   mkdir $KINDLE_HOME_DIR/books
-  ebook-convert $KINDLE_HOME_DIR/src/recipes/$1.recipe $KINDLE_HOME_DIR/books/$1.mobi --output-profile kindle --test -vv --debug-pipeline $KINDLE_HOME_DIR/debug
+  ebook-convert $KINDLE_HOME_DIR/src/recipes/$1.recipe $KINDLE_HOME_DIR/books/$1.epub --output-profile kindle --test -vv --debug-pipeline $KINDLE_HOME_DIR/debug
 }
 
 testEmail() {
   for var in "$@"
   do
-    sendEmail $KINDLE_HOME_DIR/books/$var.mobi $var
+    sendEmail $KINDLE_HOME_DIR/books/$var.epub $var
   done
 }
