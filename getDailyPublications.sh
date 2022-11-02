a05b986968e3239cfb7fe16d001899765f967b04
Gabriel Salas <ggsalas@gmail.com>
2022-11-01 22:13:47 -0300

    restore getDailyPublications and fix some recipes

--- getDailyPublications.sh
+++ getDailyPublications.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# if [ $(date +%u) -eq 5 ]; then
-#   echo ">>>>>>>>>>>>>>>>>>>>> Get Viernes ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
-#   sh $KINDLE_HOME_DIR/kindle.sh clarin lacapital lanacion perfil blogs
-#
-# # elif [ $(date +%u) -eq 7 ]; then
-# #   echo ">>>>>>>>>>>>>>>>>>>>> Get Sunday ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
-# #   sh $KINDLE_HOME_DIR/kindle.sh lanacion clarin perfil
-#
-# else
-#   echo ">>>>>>>>>>>>>>>>>>>>> Get daily News ($(date +%u)) <<<<<<<<<<<<<<<<<<<<<"
-#   sh $KINDLE_HOME_DIR/kindle.sh clarin lacapital lanacion perfil
-# fi
+if [ $(date +%u) -eq 5 ]; then
+  echo ">>>>>>>>>>>>>>>>>>>>> Get Viernes ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
+  sh $KINDLE_HOME_DIR/kindle.sh clarin lacapital lanacion perfil blogs
 
-sh $KINDLE_HOME_DIR/kindle.sh lacapital
+# elif [ $(date +%u) -eq 7 ]; then
+#   echo ">>>>>>>>>>>>>>>>>>>>> Get Sunday ($(date +%u)) News <<<<<<<<<<<<<<<<<<<<<"
+#   sh $KINDLE_HOME_DIR/kindle.sh lanacion clarin perfil
 
+else
+  echo ">>>>>>>>>>>>>>>>>>>>> Get daily News ($(date +%u)) <<<<<<<<<<<<<<<<<<<<<"
+  sh $KINDLE_HOME_DIR/kindle.sh clarin lacapital lanacion perfil
+fi
